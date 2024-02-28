84bfe855517d716450446d4e78383dadfc4d084f
mahdimortuza <mahdi.webx@gmail.com>
2024-02-28 22:23:40 +0600

    fetch added

--- src/components/leaderBoardCard/LeaderBoardCard.tsx
+++ src/components/leaderBoardCard/LeaderBoardCard.tsx
@@ -1,10 +1,16 @@
 // LeaderBoardCard.jsx
 
-const LeaderBoardCard = ({ donor }) => {
+import { Button } from "../ui/button";
+
+const LeaderBoardCard = ({ item }) => {
+  const { userName, amount } = item;
   return (
-    <div className="bg-white p-4 rounded-lg shadow">
-      <p className="text-lg font-semibold text-gray-800">{donor.username}</p>
-      <p className="text-gray-600 mt-2">${donor.amount} Donation</p>
+    <div className="bg-white p-4 rounded-lg shadow flex justify-between  items-center">
+      <div>
+        <p className="text-lg font-semibold text-gray-800">{userName}</p>
+        <p className="text-gray-600 mt-2">${amount} Donation</p>
+      </div>
+      <Button>Details</Button>
     </div>
   );
 };
--- src/pages/leaderBoard/LeaderBoard.tsx
+++ src/pages/leaderBoard/LeaderBoard.tsx
@@ -1,28 +1,26 @@
+// LeaderBoard.jsx
+
 import Container from "@/components/Container";
 import LeaderBoardCard from "@/components/leaderBoardCard/LeaderBoardCard";
+import SectionTitle from "@/components/sectionTitle/SectionTitle";
 import { useEffect, useState } from "react";
 
 const LeaderBoard = () => {
   const [topDonors, setTopDonors] = useState([]);
 
   useEffect(() => {
-    // Fetch top donors data from your API or storage
-    // For demonstration, initializing with some sample data
-    setTopDonors([
-      { id: 1, username: "GenerousUser1", amount: 1000 },
-      { id: 2, username: "KindSupporter2", amount: 800 },
-      { id: 3, username: "BigHeart3", amount: 700 },
-    ]);
+    fetch("http://localhost:5000/api/v1/donate")
+      .then((response) => response.json())
+      .then((data) => setTopDonors(data.message));
   }, []);
+
   return (
-    <Container className="py-5">
-      <div>
-        <h2 className="text-3xl font-semibold mb-4 text-gray-800">
-          Donors Leaderboard
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {topDonors.map((donor) => (
-            <LeaderBoardCard key={donor.id} donor={donor} />
+    <Container className="py-8">
+      <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-md">
+        <SectionTitle title="Leader Board" des="Top Donors Recognition" />
+        <div className="grid grid-cols-1  gap-8 mt-6">
+          {topDonors.map((item) => (
+            <LeaderBoardCard key={item.userName} item={item} />
           ))}
         </div>
       </div>
